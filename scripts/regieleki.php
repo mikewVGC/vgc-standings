44949dd9732043a280aaa15e223ef2b4a67b0407
Mike W <mike.watson+vgc@gmail.com>
2026-02-14 14:28:39 -0500

    Maybe run your code before pushing it?

--- scripts/regieleki.php
+++ scripts/regieleki.php
@@ -129,7 +129,7 @@ while (1) {
             $attempts++;
 
             // backoff starts at +1 minute
-            $backoff = $sleep_time * pow(2, $attempts);
+            $backoff = $refresh_rate * pow(2, $attempts);
 
             elog_cont("Invalid JSON, starting backoff...");
             break;
