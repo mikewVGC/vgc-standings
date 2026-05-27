f7968ad466d64c0365df04ee12a92ac416e72a77
Mike W <mike.watson+vgc@gmail.com>
2026-05-27 11:01:40 -0400

    Update packer to read from build.json

--- scripts/packer/main.go
+++ scripts/packer/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"regexp"
+	"encoding/json"
 
 	"github.com/tdewolff/minify/v2"
 	"github.com/tdewolff/minify/v2/css"
@@ -19,23 +21,49 @@ func main() {
 	// many thanks to tdewolff/minify
 	// https://github.com/tdewolff/minify
 
+	buildData, fileErr := os.ReadFile("build.json")
+	if fileErr != nil {
+		log.Printf("Unable to read build.json")
+	}
+
+	buildInfo := make(map[string]interface{})
+	jsonErr := json.Unmarshal(buildData, &buildInfo)
+	if jsonErr != nil {
+		log.Printf("JSON parse error (build.json): %s", jsonErr)
+	}
+
+	packInfo := buildInfo["packer"].(map[string]interface{})
+
+	cssFiles := packInfo["css"].(map[string]interface{})
+	jsFiles := packInfo["js"].(map[string]interface{})
+
 	m = minify.New()
 	m.AddFunc("text/css", css.Minify)
 	m.AddFuncRegexp(regexp.MustCompile("^(application|text)/(x-)?(java|ecma)script$"), js.Minify)
 
 	log.Printf("Minifying CSS")
-	loadAndMinify("css/style.css", "style.min.css", "text/css")
+	for inCssFile, outCssFile := range cssFiles {
+		loadAndMinify(
+			fmt.Sprintf("%s/%s", packInfo["tpl_dir"], inCssFile),
+			fmt.Sprintf("%s/%s", packInfo["out_dir"], outCssFile),
+			"text/css",
+		)
+	}
 
 	log.Printf("Minifying JS")
-	loadAndMinify("js/home.js", "home.min.js", "application/javascript")
-	loadAndMinify("js/season.js", "season.min.js", "application/javascript")
-	loadAndMinify("js/tournament.js", "tournament.min.js", "application/javascript")
+	for inJsFile, outJsFile := range jsFiles {
+		loadAndMinify(
+			fmt.Sprintf("%s/%s", packInfo["tpl_dir"], inJsFile),
+			fmt.Sprintf("%s/%s", packInfo["out_dir"], outJsFile),
+			"application/javascript",
+		)
+	}
 
 	log.Printf("Done")
 }
 
 func loadAndMinify(filename string, minFilename string, mediaType string) error {
-	dat, err := os.ReadFile("site/" + filename)
+	dat, err := os.ReadFile(filename)
 	if err != nil {
 		log.Printf("Unable to open %s: %v", filename, err)
 		return err
@@ -47,7 +75,7 @@ func loadAndMinify(filename string, minFilename string, mediaType string) error 
 		return err
 	}
 
-	err = os.WriteFile("public/static/" + minFilename, minDat, 0644);
+	err = os.WriteFile(minFilename, minDat, 0644);
 	if err != nil {
 		log.Printf("Unable to write minified %s: %v", minFilename, err)
 		return err
