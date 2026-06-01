6a10b13ea1c21e8035820402cf0adc756e899a12
Mike W <mike.watson+vgc@gmail.com>
2026-05-31 23:11:43 -0400

    Forgot to add alt form stuff so megas show up on teamsheets

--- scripts/ops/processors/rk9scraper.py
+++ scripts/ops/processors/rk9scraper.py
@@ -59,11 +59,14 @@ def process_rk9scraper_event(
                 mon_name = "Maushold"
 
             mon_code = make_mon_code(mon_name)
+            mon_alt_code = get_icon_alt(mon_code, mon, event_info['rules']['mega'])
             dex_num, ptype = get_mon_data_from_code(mon_code)
 
-            alt = get_mon_alt_from_code(mon_code)
+            alt = get_mon_alt_from_code(mon_alt_code) if mon_alt_code else get_mon_alt_from_code(mon_code)
             if alt:
                 dex_num = alt
+
+            mon_item = mon['item'] if len(mon['item']) else 'No Item'
 
             team.append(TeamMember(
                 name=mon_name,
@@ -75,8 +78,8 @@ def process_rk9scraper_event(
                 tera=mon['tera'],
                 ability=mon['ability'],
                 nature=mon['nature'],
-                item=mon['item'],
-                itemcode=make_item_code(mon['item']),
+                item=mon_item,
+                itemcode=make_item_code(mon_item),
                 moves=mon['moves'],
             ))
 
