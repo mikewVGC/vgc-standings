f52ad228c311277f3436fce939b36f622dd57aaf
Mike W <mike.watson+vgc@gmail.com>
2026-07-14 19:15:59 -0400

    Limitless fetcher

---
+++ scripts/limitless_fetch.php
@@ -0,0 +1,46 @@
+<?php
+
+// This is just because I'm lazy and want to grab limitless data easily
+// this just does a basic fetch of the standings/pairings/details
+
+// usage: php limitless.php [tour hash] [tour code]
+
+if ($argc != 3) {
+    echo "Usage php limitless.php [tour hash] [tour code]\n";
+    exit(1);
+}
+
+$hash = $argv[1];
+$code = $argv[2];
+
+// assumes you run fron the root
+$save_location = realpath(__DIR__ . '/../data/majors/limitless');
+
+$base_url = "https://play.limitlesstcg.com/api/tournaments";
+
+$sections = [
+    'details',
+    'pairings',
+    'standings',
+];
+
+foreach ($sections as $section) {
+    $url = "{$base_url}/{$hash}/{$section}";
+
+    echo "Fetching {$section} data...";
+
+    $response = file_get_contents($url);
+    if ($response === false) {
+        echo "Download of '{$section}' failed! Skipping.\n";
+        continue;
+    }
+
+    echo "Done!\n";
+
+    file_put_contents("{$save_location}/{$code}-{$section}.json", $response);
+}
+
+echo "Don't forget to update limitless.json\n";
+
+echo "Done, good bye!\n";
+exit(0);
